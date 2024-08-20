cc1eb1a8dd31ca2b1620c6f44281d0b5f498ffaa
jenscodejens <161465689+jenscodejens@users.noreply.github.com>
2024-08-20 14:02:30 +0200

    Delete cms-hackers-heroes directory

--- cms-hackers-heroes/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace cms_hackers_heroes.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
